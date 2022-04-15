ae978c3884194531d159294b7f3b17140d748ec5
Shubham Kumar Shukla <shubhssays@gmail.com>
2022-04-15 14:51:05 +0530

    Configuring MongoDb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import 'express-async-errors'
-import {json} from 'body-parser'
+import { json } from 'body-parser'
+import mongoose from 'mongoose'
+
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
-import { signoutRouter } from  './routes/signout'
+import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
@@ -16,9 +18,9 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*',async () =>{
+app.all('*', async () => {
     throw new NotFoundError
-}) 
+})
 
 // app.all('*',async (req, res, next) =>{
 //     next(new NotFoundError)
@@ -26,8 +28,18 @@ app.all('*',async () =>{
 
 app.use(errorHandler)
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to mongodb')
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
 
-
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Listening on port 3000 !!!!!!')
 })
+
+start();
