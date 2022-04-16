88b46e6242e007e01882aa92e030ad680cd4a37a
Shubham Kumar Shukla <shubhssays@gmail.com>
2022-04-16 14:20:17 +0530

    First Test Sign up User passed

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+
+
+import { signupRouter } from './routes/signup'
+import { signinRouter } from './routes/signin'
+import { currentUserRouter } from './routes/current-user'
+import { signoutRouter } from './routes/signout'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+
+app.use(signupRouter)
+app.use(signinRouter)
+app.use(currentUserRouter)
+app.use(signoutRouter)
+
+app.all('*', async () => {
+    throw new NotFoundError
+})
+
+// app.all('*',async (req, res, next) =>{
+//     next(new NotFoundError)
+// }) 
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,6 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
+
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-
-
-import { signupRouter } from './routes/signup'
-import { signinRouter } from './routes/signin'
-import { currentUserRouter } from './routes/current-user'
-import { signoutRouter } from './routes/signout'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(currentUserRouter)
-app.use(signoutRouter)
-
-app.all('*', async () => {
-    throw new NotFoundError
-})
-
-// app.all('*',async (req, res, next) =>{
-//     next(new NotFoundError)
-// }) 
-
-app.use(errorHandler)
+import { app } from './app';
 
 const start = async () => {
 
