c4c53b5e3ac6ec584380d80c74f2119f6bb0460a
Shubham Kumar Shukla <shubhssays@gmail.com>
2022-04-16 12:17:55 +0530

    Sign up, Sign In and Current User

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error'
+import { body } from 'express-validator'
 import { User } from '../models/user'
 import { BadRequestError } from '../errors/bad-request.error'
+import { validateRequest } from '../middlewares/validate-request'
 import jwt from 'jsonwebtoken'
 
 const router = express.Router()
@@ -11,14 +11,7 @@ const router = express.Router()
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        // return res.status(400).send(errors.array())
-        //throw new Error('Invalid email or password')
-        throw new RequestValidationError(errors.array())
-    }
-
+], validateRequest, async (req: Request, res: Response) => {
     const { email, password } = req.body
     const existingUser = await User.findOne({ email })
     if(existingUser){
