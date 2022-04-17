6c972e0b422484ccaeb5404c31abd50540da3a92
Shubham Kumar Shukla <shubhssays@gmail.com>
2022-04-17 14:17:00 +0530

    Added My Npm Library for common and using it

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,8 +2,7 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { User } from '../models/user'
-import { BadRequestError } from '../errors/bad-request.error'
-import { validateRequest } from '../middlewares/validate-request'
+import { BadRequestError , validateRequest } from '@devshubhs_micro_ta/common'
 import jwt from 'jsonwebtoken'
 
 const router = express.Router()
