4c8d97f97ddaa34b3ecbc98e4ed6c727cd5f7022
Shubham Kumar Shukla <shubhssays@gmail.com>
2022-04-21 17:41:11 +0530

    Ticket Model and create ticket api done

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError } from '@devshubhs_micro_ta/common'
+import { errorHandler, NotFoundError, currentUser } from '@devshubhs_micro_ta/common'
 
-
+import { createTicketRouter } from './routes/new'
 const app = express()
 app.set('trust proxy', true)
 app.use(json())
@@ -12,6 +12,9 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }))
+
+app.use(currentUser)
+app.use(createTicketRouter)
 
 app.all('*', async () => {
     throw new NotFoundError
---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,89 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
+
+it('has a router handler listening to /api/tickets for post requests', async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .send({})
+
+  expect(response.status).not.toEqual(404)
+})
+
+it('can only be accessed if user is signed in', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .send({})
+    .expect(401)
+})
+
+it('returns a status other than 401 if the user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({})
+
+  expect(response.status).not.toEqual(401)
+})
+
+it('returns an error if invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: "",
+      price: 10
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      price: 10
+    })
+    .expect(400)
+})
+
+it('returns an error if invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: "asdsmnfk",
+      price: -10
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: "fssdgfvdghhg",
+    })
+    .expect(400)
+})
+
+it('creates a ticket with valid inputs', async () => {
+
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  const title = "sajjkfnklsf"
+  const price = 24
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({
+      title: title,
+      price: price
+    })
+    .expect(201)
+
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].title).toEqual(title)
+  expect(tickets[0].price).toEqual(price)
+})
