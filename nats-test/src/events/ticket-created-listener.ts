50f4fbb761a51bf0dc4e883a8b7e41e901d6918e
Shubham Kumar Shukla <shubhssays@gmail.com>
2022-05-11 12:59:57 +0530

    Managing Git Clients

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,8 +1,8 @@
 
 import { Message } from 'node-nats-streaming';
-import { Listener } from  '@devshubhs_micro_ta/common'
-import { TicketCreatedEvent } from '@devshubhs_micro_ta/common';
-import { Subjects } from '@devshubhs_micro_ta/common';
+import { Listener } from  './base-listener'
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     subject: Subjects.TicketCreated = Subjects.TicketCreated;
